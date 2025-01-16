e4f57920c6502ba95854affbd70490dfa3f1a4b3
GuriMustafa <gurimustafaa@gmail.com>
2025-01-16 10:46:04 +0100

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,26 @@
+const textbox =document.getElementById("textbox");
+const radioferhan=document.getElementById("radioferhan");
+const radiocelsius=document.getElementById("radiocelsius");
+const result=document.getElementById("result");
+
+let temp;
+
+
+
+
+
+function convert(){
+    if (radioferhan.checked){
+        temp = Number(textbox.value);
+        temp= temp * 9 / 5 +32;
+        result.textContent = temp.toFixed(1) + "°F"
+    }
+    else if (radiocelsius.checked){
+        temp = Number(textbox.value);
+        temp= (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C"
+    }
+    else{
+        result.textContent=("select an unit")
+    }
+}
